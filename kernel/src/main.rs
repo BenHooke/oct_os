0d2d5f4e01275298026ea996e78ca8bc943617ec
Ben Hooke <BenHooke73@live.ca>
2026-03-14 17:05:46 -0400

    Add serial unit testing with print to terminal funcitons

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,13 +1,44 @@
 #![no_std]  // Don't link the Rust standard library
 #![no_main]  // Disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[ failed ]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -15,5 +46,38 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Test {}", "print.");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    let mut grammar = "tests";
+    if tests.len() <= 1 {
+        grammar = "test";
+    } 
+
+    serial_println!("Running {} {}", tests.len(), grammar);
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("Trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ ok ]");
+}
+
+#[test_case]
+fn trivial_assertion_2() {
+    serial_print!("Trivial assertion 2... ");
+    assert_eq!(2, 2);
+    serial_println!("[ ok ]");
 }
