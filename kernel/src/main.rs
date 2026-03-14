341335e8bea1246eaec3821ca4a53202d5295793
Ben Hooke <BenHooke73@live.ca>
2026-03-14 14:53:21 -0400

    Remove profanities from main.rs

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -11,19 +11,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// static FUCK: &[u8] = b"Fuck you.";
-
 #[unsafe(no_mangle)]  // Don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_something();
-    // let vga_buffer = 0xb8000 as *mut u8;
-    //
-    // for (i, &byte) in FUCK.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
 
     loop {}
 }
